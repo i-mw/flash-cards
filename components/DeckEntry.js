ea10cb7b403a7fa55cccf6e78446a70140a4181e
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2020-04-24 23:35:25 +0200

    feat: Style deck list view

--- components/DeckEntry.js
+++ components/DeckEntry.js
@@ -1,15 +1,38 @@
 import React from "react";
-import { View, Text, TouchableNativeFeedback, TouchableOpacity } from "react-native";
-import {useNavigation} from '@react-navigation/native'
+import {
+  View,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Styled from "styled-components/native";
+
+const Container = Styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  height: 150px;
+  border-style: solid;
+  border-bottom-width: 1px;
+  border-bottom-color: gray;
+`;
+
+const Heading = Styled.Text`
+  font-size: 1.2em;
+  margin-bottom: 10px;
+`;
+
+const SubHeading = Styled.Text`
+  font-size: 0.8em;
+  color: gray;
+`;
 
 export default function DeckEntry(props) {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('DeckDetails')}>
-      <View>
-        <Text>A deck title</Text>
-        <Text>3 cards</Text>
-      </View>
-    </TouchableOpacity>
+    <Container onPress={() => navigation.navigate("DeckDetails")}>
+      <Heading>A deck title</Heading>
+      <SubHeading>3 cards</SubHeading>
+    </Container>
   );
 }
--- components/DeckList.js
+++ components/DeckList.js
@@ -4,8 +4,9 @@ import DeckEntry from "./DeckEntry";
 import { createStackNavigator } from "@react-navigation/stack";
 
 export default function DeckList(props) {
-  console.log(props);
   const Data = [0, 1, 2, 3, 4, 5, 6, 7];
 
-  return <FlatList data={Data} renderItem={({ item }) => <DeckEntry navigation={props.navigation} key={item}/>} />;
+  return (
+    <FlatList style={{flex: 1}} data={Data} renderItem={({ item }) => <DeckEntry navigation={props.navigation} key={item}/>} />
+  );
 }
