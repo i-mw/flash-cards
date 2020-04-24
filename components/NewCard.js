fe9f764f71ca0f5801368e959eb9d039c7c7c8db
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2020-04-24 23:57:15 +0200

    feat: Style new card view

--- components/NewCard.js
+++ components/NewCard.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React from "react";
 import { View, Text, TextInput, Button } from "react-native";
+import Styled from "styled-components/native";
 
-export default function NewCard({navigation}) {
+const Container = Styled.View`
+  flex: 1;
+  padding: 30px;
+  padding-top: 50px;
+  align-items: center;
+`;
+const Input = Styled.TextInput`
+  margin-bottom: 25px;
+  border: 1px solid gray
+  padding: 10px;
+  font-size: 1.1em;
+`;
+const SubmitBtn = Styled.View`
+  margin-top: 15px;
+  width: 40%;
+`;
+
+export default function NewCard({ navigation }) {
   return (
-    <View>
-      <TextInput placeholder="Enter the question"/>
-      <TextInput placeholder="Enter the answer"/>
-      <Button title="Submit" onPress={() => navigation.navigate('DeckDetails')}/>
-    </View>
-  )
+    <Container>
+      <Input placeholder="Enter the question" />
+      <Input placeholder="Enter the answer" />
+      <SubmitBtn>
+        <Button
+          disabled={false}
+          title="Submit"
+          onPress={() => navigation.navigate("DeckDetails")}
+        />
+      </SubmitBtn>
+    </Container>
+  );
 }
