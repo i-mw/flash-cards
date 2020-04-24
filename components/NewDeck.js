333e4fb921ed7a900aea72f60fd4c8ce4fb196d4
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2020-04-24 23:34:43 +0200

    feat: Style new deck view

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import React from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import Styled from "styled-components/native";
 
-export default function NewDeck({navigation}) {
+const Container = Styled.View`
+  flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 30px;
+  padding-top: 50px;
+`;
 
+const Heading = Styled.Text`
+  font-size: 2.5em;
+  text-align: center;
+`;
+
+const Input = Styled.TextInput`
+  margin-top: 70px;
+  border: 1px solid gray
+  padding: 10px;
+  font-size: 1.1em;
+  `;
+
+const SubmitBtn = Styled.View`
+  margin-top: 30px;
+  width: 40%;
+`;
+
+export default function NewDeck({ navigation }) {
   return (
-    <View>
-      <Text>What is the title of your new deck?</Text>
-      <TextInput placeholder="Deck name"/>
-      <Button title="Submit" onPress={() => navigation.navigate('DeckDetails')}/>
-    </View>
-  )
+    <Container>
+      <Heading>What is the title of your new deck?</Heading>
+      <Input placeholder="Enter Deck Name" />
+      <SubmitBtn>
+        <Button
+          disabled={false}
+          title="Submit"
+          onPress={() => navigation.navigate("DeckDetails")}
+        />
+      </SubmitBtn>
+    </Container>
+  );
 }
