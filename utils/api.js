29fc669097fb82b860607b17930b403c0ebe3211
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2020-04-25 07:39:58 +0200

    feat: Add all api methods

---
+++ utils/api.js
@@ -0,0 +1,50 @@
+import {AsyncStorage} from "react-native";
+
+function generateId() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
+
+export function getDecks() {
+  return AsyncStorage.getItem("decks")
+    .then(JSON.parse)
+    .catch((e) => e);
+}
+
+export function saveDeck(title) {
+  const deckId = generateId();
+  const deck = {
+    id: deckId,
+    title,
+    cards: {},
+  };
+
+  return AsyncStorage.mergeItem("decks", JSON.stringify({ [deckId]: deck }))
+    .then(() => deck)
+    .catch((e) => e);
+}
+
+export function removeDeck(deckId) {
+  return getDecks().then((decks) => {
+    delete decks[deckId];
+    return AsyncStorage.setItem("decks", JSON.stringify(decks));
+  });
+}
+
+export function saveCard({ deckId, question, answer }) {
+  const cardId = generateId();
+  const card = {
+    id: cardId,
+    question,
+    answer,
+  };
+
+  return getDecks().then((decks) => {
+    decks[deckId].cards[cardId] = card;
+    return AsyncStorage.setItem("decks", JSON.stringify(decks)).then(
+      () => card
+    );
+  });
+}
